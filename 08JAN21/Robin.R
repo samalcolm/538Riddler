7b5a7ca6e5755de8973f3897c1e90ca31fa7b340
samalcolm <48925544+samalcolm@users.noreply.github.com>
2021-01-11 10:16:31 -0500

    Update Robin.R

--- 08JAN21/Robin.R
+++ 08JAN21/Robin.R
@@ -1,1 +1,19 @@
 
+robin = function(){
+  score = 1
+  r1 = runif(1)
+  done = FALSE
+  while (!done){
+    r0 = r1
+    r1 = runif(1)
+    if (r1 < r0) score = score + 1 
+    else done = TRUE
+  #    x1 = y1
+}
+  score = score + 1 
+}
+
+s = sapply(c(1:1e8), function(x) robin())
+
+mean(s)
+#[1] 2.718269           
